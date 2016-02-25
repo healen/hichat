ad7e7bb311ce795f188004dcfb7a2311a7b45d1d
zhangxiaodong <zhangxiaodong@daishu360.com>
2016-02-25 11:51:10 +0800

    完成了

--- static/js/hichat.js
+++ static/js/hichat.js
@@ -3,6 +3,7 @@
  * author ZhangXiaodong
  * 2016-2-23
  */
+
 window.onload=function(){
 	var hichat=new HiChat();
 	hichat.init();
@@ -12,16 +13,17 @@ function HiChat(){
 }
 HiChat.prototype={
 	init:function(){
-		var ELE_NICKNAME = $("#username");
+		var ELE_NICKNAME  = $("#username");
 		var ELE_USERLOGIN = $("#userlogin");
-		var ELE_LOGINBOX = $("#loginbox");
-		var ELE_CHATBOX = $("#chatbox");
-		var ELE_USERLIST = $("#userlist");
-		var ELE_NUM = $("#num");
-		var ELE_SHOWBOX = $("#showbox");
-		var ELE_MYUSER = $("#myuser");
-		var ELE_SEND = $("#send");
-		var ELE_MSGBOX = $("#msgbox");
+		var ELE_LOGINBOX  = $("#loginbox");
+		var ELE_CHATBOX   = $("#chatbox");
+		var ELE_USERLIST  = $("#userlist");
+		var ELE_NUM       = $("#num");
+		var ELE_SHOWBOX   = $("#showbox");
+		var ELE_MYUSER    = $("#myuser");
+		var ELE_SEND      = $("#send");
+		var ELE_MSGBOX    = $("#msgbox");
+		var ELE_SHOWMSG   =$(".showmsg")
 		var that          =this;
 		var loading       =layer.load();
 		this.userid=null;
@@ -66,18 +68,24 @@ HiChat.prototype={
 		})
 
 		this.socket.on("newMsg",function(msg,userName,users,pageuser){
-			console.log(pageuser+" "+ users.indexOf(userName));
+			console.log(userName+" "+ ELE_MYUSER.text());
 
-			var myuser = pageuser== users.indexOf(userName) ? "myuser" : "service"
+			var sendDate = new Date().Format("yyyy-MM-dd hh:mm:ss");
 
+			var isme = userName== ELE_MYUSER.text() ? "me" : ""
 
-
-			var html = '<div className="msglist '+myuser+'">' +userName+':'+msg+'</div>';
-
+			var html 	  = '<div class="row">';
+				html     += '<div class="msglist '+isme+'">';
+				html     += '<img src="/static/images/empty_head.png" />';
+				html     += '<div class="msgwarp">';
+				html     += '<div class="titles">'+userName+' ('+sendDate+')</div>';
+				html     += '<div class="contentmsg">';
+				html     += '<div class="sanjiao"></div>';
+				html     += '<span>'+msg+'</span>';
+				html     += '</div></div></div></div>';
 			ELE_SHOWBOX.append(html);
-
-		
-			
+			ELE_MSGBOX.val('');
+			ELE_SHOWMSG.animate({scrollTop: ELE_SHOWBOX.height()}, 300)
 		})
 
 		ELE_USERLOGIN.on("click",function(){
@@ -98,10 +106,18 @@ HiChat.prototype={
 			var msgContent=ELE_MSGBOX.val();
 			msgContent=msgContent.replace(/\n/g,"<br\/>");
 			msgContent=msgContent.replace(/\s/g,"&nbsp;");
-			// console.log(msgContent);
-			that.socket.emit("send msg",msgContent);
-
 			
+			if(msgContent.length > 0){
+				that.socket.emit("send msg",msgContent);
+			}
+			ELE_MSGBOX.focus();
+		})
+
+		ELE_MSGBOX.on("keypress",function(e){
+			if(e.ctrlKey && e.which == 13 || e.which == 10) { 
+				ELE_SEND.trigger("click");
+			}
+
 		})
 
 	}
